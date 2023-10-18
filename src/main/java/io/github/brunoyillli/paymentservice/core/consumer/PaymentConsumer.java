5ffba631dcda215d0cb48dc40d66c8a51a10adf2
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-10-18 13:26:56 -0300

    criando consumer e producer

---
+++ src/main/java/io/github/brunoyillli/paymentservice/core/consumer/PaymentConsumer.java
@@ -0,0 +1,40 @@
+package io.github.brunoyillli.paymentservice.core.consumer;
+
+
+
+
+import io.github.brunoyillli.paymentservice.core.dto.Event;
+import io.github.brunoyillli.paymentservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class PaymentConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-success}"
+    )
+    public void consumerSuccessEvent(String payload){
+        log.info("Receiving success event {} from payment-success topic", payload);
+        Event event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-fail}"
+    )
+    public void consumerFailEvent(String payload){
+        log.info("Receiving rollback event {} from payment-fail topic", payload);
+        Event event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+}
